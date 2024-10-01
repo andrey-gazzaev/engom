b190f69300573fa9c469589e88357d9018462841
Andrey Gazzaev <andrey.gazzaev@saritasa.com>
2024-10-01 13:34:28 +0700

    Update database models JC-511

--- db/init/00-database.sql
+++ db/init/00-database.sql
@@ -25,5 +25,43 @@ CREATE TABLE public.group (
 CREATE TABLE public.groupUser (
     user_id INTEGER REFERENCES public."user" (id),
     group_id INTEGER REFERENCES public.group (id),
-		primary key (user_id, group_id)
+    primary key (user_id, group_id)
+);
+
+/* Create task table in public schema */
+CREATE TABLE public.task (
+    id SERIAL PRIMARY KEY,
+    description varchar(255) NOT NULL,
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+/* Create groupTask table in public schema */
+CREATE TABLE public.groupTask (
+    task_id INTEGER REFERENCES public.task (id),
+    group_id INTEGER REFERENCES public.group (id),
+    lastSentAt TIMESTAMP DEFAULT NULL,
+    primary key (task_id, group_id)
+);
+
+/* Create userTask table in public schema */
+CREATE TABLE public.userTask (
+    task_id INTEGER REFERENCES public.task (id),
+    user_id INTEGER REFERENCES public."user" (id),
+    completedAt TIMESTAMP DEFAULT NULL,
+    primary key (user_id, task_id)
+);
+
+/* Create task table in public schema */
+CREATE TABLE public.vocabulary (
+    id SERIAL PRIMARY KEY,
+    origin varchar(255) NOT NULL,
+    translation varchar(255) NOT NULL,
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+/* Create vocabularyTask table in public schema */
+CREATE TABLE public.vocabularyTask (
+    vocabulary_id INTEGER REFERENCES public.vocabulary (id),
+    task_id INTEGER REFERENCES public.task (id),
+    primary key (vocabulary_id, task_id)
 );
