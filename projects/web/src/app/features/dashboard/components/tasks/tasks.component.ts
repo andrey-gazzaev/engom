7abedfce44f58fa0bc9dae9db4dfbce7bdb18af1
Andrey Gazzaev <andrey.gazzaev@saritasa.com>
2024-10-18 16:39:50 +0700

    Add docs JC-511

--- projects/web/src/app/features/dashboard/components/tasks/tasks.component.ts
+++ projects/web/src/app/features/dashboard/components/tasks/tasks.component.ts
@@ -14,18 +14,24 @@ export class TasksComponent {
 	/** Tasks. */
 	public readonly tasks = model<readonly Task[]>([]);
 
+	/** Emits when uncomplete button is clicked. */
 	public readonly uncompleteClicked = output<Task>();
 
+	/** Emits when complete button is clicked. */
 	public readonly completeClicked = output<Task>();
 
 	/**
-	 * Handles
-	 * @param task
+	 * Handles a click on the uncomplete button.
+	 * @param task Task to be uncompleted.
 	 */
 	protected onUncompleteClick(task: Task): void {
 		this.uncompleteClicked.emit(task);
 	}
 
+	/**
+	 * Handles a click on the complete button.
+	 * @param task Task to be completed.
+	 */
 	protected onCompleteClick(task: Task): void {
 		this.completeClicked.emit(task);
 	}
